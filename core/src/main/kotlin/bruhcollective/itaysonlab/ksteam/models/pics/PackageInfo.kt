ef1a58fb9f2fb7649e039387b0f3bbae2178ac8e
iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>
2023-03-06 22:42:00 -0600

    model: expand packageinfo
    Signed-off-by: iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>

--- core/src/main/kotlin/bruhcollective/itaysonlab/ksteam/models/pics/PackageInfo.kt
+++ core/src/main/kotlin/bruhcollective/itaysonlab/ksteam/models/pics/PackageInfo.kt
@@ -17,12 +17,15 @@ data class PackageInfo internal constructor(
     data class PackageInfoExtended internal constructor(
         @SerialName("allowcrossregiontradingandgifting") val allowInternationalGifting: Boolean = false,
         @SerialName("allowpurchasefromrestrictedcountries") val allowPurchaseFromRestricted: Boolean = false,
-        @SerialName("purchaserestrictedcountries") val restrictedCountries: String = "",
+        @SerialName("onlyallowrestrictedcountries") val onlyAllowRestrictedCountries: Boolean = false,
+        @SerialName("purchaserestrictedcountries") val purchaseRestrictedCountries: String = "",
+        @SerialName("restrictedcountries") val restrictedCountries: String = "",
         @SerialName("basepackage") val basePackageId: Int? = null,
         @SerialName("dontgrantifappidowned") val dontGrantIfAppOwned: Int? = null,
         @SerialName("starttime") val startTime: Int = 0,
         @SerialName("expirytime") val expiryTime: Int = 0,
         @SerialName("freepromotion") val freePromotion: Boolean = false,
         @SerialName("disabletradingcards") val disableTradingCards: Boolean = false,
+        @SerialName("excludefromsharing") val excludeFromFamilySharing: Boolean = false,
     )
 }
