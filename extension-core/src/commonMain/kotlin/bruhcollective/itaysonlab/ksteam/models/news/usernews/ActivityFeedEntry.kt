7f9873a6e0e25590163b4da6a3bc6135632400f0
iTaysonLab <itaysontesterlab@gmail.com>
2023-06-15 14:45:36 -0600

    extension-core/news: unify activity feed entry and introduce unique ID for UI optimizations

--- extension-core/src/commonMain/kotlin/bruhcollective/itaysonlab/ksteam/models/news/usernews/ActivityFeedEntry.kt
+++ extension-core/src/commonMain/kotlin/bruhcollective/itaysonlab/ksteam/models/news/usernews/ActivityFeedEntry.kt
@@ -1,9 +1,8 @@
 package bruhcollective.itaysonlab.ksteam.models.news.usernews
 
-import bruhcollective.itaysonlab.ksteam.models.SteamId
 import bruhcollective.itaysonlab.ksteam.models.apps.AppSummary
 import bruhcollective.itaysonlab.ksteam.models.enums.EUserNewsType
-import bruhcollective.itaysonlab.ksteam.models.news.NewsEntry
+import bruhcollective.itaysonlab.ksteam.models.news.NewsEvent
 import bruhcollective.itaysonlab.ksteam.models.persona.SummaryPersona
 import bruhcollective.itaysonlab.ksteam.platform.Immutable
 import steam.webui.usernews.CUserNews_Event
@@ -12,11 +11,12 @@ import kotlin.contracts.contract
 
 @Immutable
 sealed class ActivityFeedEntry (
-    val date: Int,
-    val steamId: SteamId,
-    val persona: SummaryPersona,
+    val id: String,
+    val date: Int
 ) {
-    companion object {
+    internal companion object {
+        fun buildId(date: Int, persona: SummaryPersona, extra: String) = "fa_${persona.id}_$date:$extra"
+
         @OptIn(ExperimentalContracts::class)
         fun defaultMatch(event: CUserNews_Event, eventNext: CUserNews_Event?, type: EUserNewsType): Boolean {
             contract {
@@ -28,17 +28,15 @@ sealed class ActivityFeedEntry (
     }
 
     /**
-     * A new blog post was posted about this app.
+     * A synthetic entry which holds a [NewsEvent]. Useful for unified feeds.
      */
     @Immutable
-    class PostedAnnouncement(
+    class NewsfeedEvent(
         date: Int,
-        steamId: SteamId,
-        persona: SummaryPersona,
-        val announcement: NewsEntry
-    ): ActivityFeedEntry(date, steamId, persona) {
+        val event: NewsEvent
+    ): ActivityFeedEntry(id = event.id, date) {
         override fun toString(): String {
-            return "PostedAnnouncement(date=$date, steamId=$steamId, persona=$persona, announcement=$announcement)"
+            return "NewsfeedEvent(date=$date, event=$event"
         }
     }
 
@@ -48,17 +46,16 @@ sealed class ActivityFeedEntry (
     @Immutable
     class ReceivedNewGame(
         date: Int,
-        steamId: SteamId,
-        persona: SummaryPersona,
+        val persona: SummaryPersona,
         val apps: List<AppSummary>,
         val packages: List<AppSummary>,
-    ): ActivityFeedEntry(date, steamId, persona) {
+    ): ActivityFeedEntry(id = buildId(date, persona, "rn_${apps.hashCode()}+${packages.hashCode()}"), date) {
         companion object {
             fun canMergeWith(event: CUserNews_Event, eventNext: CUserNews_Event?) = defaultMatch(event, eventNext, EUserNewsType.ReceivedNewGame)
         }
 
         override fun toString(): String {
-            return "ReceivedNewGame(date=$date, steamId=$steamId, persona=$persona, apps=${apps.joinToString()}, packages=${packages.joinToString()})"
+            return "ReceivedNewGame(date=$date, persona=$persona, apps=${apps.joinToString()}, packages=${packages.joinToString()})"
         }
     }
 
@@ -68,16 +65,15 @@ sealed class ActivityFeedEntry (
     @Immutable
     class AddedToWishlist(
         date: Int,
-        steamId: SteamId,
-        persona: SummaryPersona,
+        val persona: SummaryPersona,
         val apps: List<AppSummary>
-    ): ActivityFeedEntry(date, steamId, persona) {
+    ): ActivityFeedEntry(id = buildId(date, persona, "aw_${apps.hashCode()}"), date) {
         companion object {
             fun canMergeWith(event: CUserNews_Event, eventNext: CUserNews_Event?) = defaultMatch(event, eventNext, EUserNewsType.AddedGameToWishlist)
         }
 
         override fun toString(): String {
-            return "AddedToWishlist(date=$date, steamId=$steamId, persona=$persona, apps=${apps.joinToString()})"
+            return "AddedToWishlist(date=$date, persona=$persona, apps=${apps.joinToString()})"
         }
     }
 
@@ -87,12 +83,11 @@ sealed class ActivityFeedEntry (
     @Immutable
     class PlayedForFirstTime(
         date: Int,
-        steamId: SteamId,
-        persona: SummaryPersona,
+        val persona: SummaryPersona,
         val app: AppSummary
-    ): ActivityFeedEntry(date, steamId, persona) {
+    ): ActivityFeedEntry(id = buildId(date, persona, "ft_${app.id}"), date) {
         override fun toString(): String {
-            return "PlayedForFirstTime(date=$date, steamId=$steamId, persona=$persona, app=$app)"
+            return "PlayedForFirstTime(date=$date, persona=$persona, app=$app)"
         }
     }
 
@@ -102,11 +97,10 @@ sealed class ActivityFeedEntry (
     @Immutable
     class NewAchievements(
         date: Int,
-        steamId: SteamId,
-        persona: SummaryPersona,
+        val persona: SummaryPersona,
         val app: AppSummary,
         val achievements: List<Achievement>
-    ): ActivityFeedEntry(date, steamId, persona) {
+    ): ActivityFeedEntry(id = buildId(date, persona, "ac_${app.id}+${achievements.hashCode()}"), date) {
         companion object {
             fun canMergeWith(event: CUserNews_Event, eventNext: CUserNews_Event?): Boolean {
                 return defaultMatch(event, eventNext, EUserNewsType.AchievementUnlocked) && eventNext.gameid == event.gameid
@@ -123,7 +117,7 @@ sealed class ActivityFeedEntry (
         )
 
         override fun toString(): String {
-            return "NewAchievements(date=$date, steamId=$steamId, persona=$persona, app=$app, achievements=[${achievements.joinToString()}])"
+            return "NewAchievements(date=$date, persona=$persona, app=$app, achievements=[${achievements.joinToString()}])"
         }
     }
 
@@ -133,13 +127,12 @@ sealed class ActivityFeedEntry (
     @Immutable
     class UnknownEvent(
         date: Int,
-        steamId: SteamId,
-        persona: SummaryPersona,
+        val persona: SummaryPersona,
         val type: EUserNewsType,
         val proto: CUserNews_Event
-    ): ActivityFeedEntry(date, steamId, persona) {
+    ): ActivityFeedEntry(id = buildId(date, persona, "unk_${proto.hashCode()}"), date) {
         override fun toString(): String {
-            return "UnknownEvent(date=$date, steamId=$steamId, persona=$persona, type=$type, proto=$proto)"
+            return "UnknownEvent(date=$date, persona=$persona, type=$type, proto=$proto)"
         }
     }
 }
