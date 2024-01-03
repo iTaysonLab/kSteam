6ef4c1e2520a6341c404afd28d8c1171b32efb61
Dmytro Tarasenko <itaysontesterlab@gmail.com>
2024-01-03 03:48:11 -0600

    Fix KotlinX VDF test building

--- kotlinx-vdf/build.gradle.kts
+++ kotlinx-vdf/build.gradle.kts
@@ -16,6 +16,7 @@ kotlin {
 androidLibrary("bruhcollective.itaysonlab.kotlinx_vdf")
 
 dependencies {
+    commonMainImplementation(kotlin("test"))
     commonMainImplementation(libs.kotlinx.serialization.core)
     commonMainImplementation(libs.okio)
     commonMainImplementation("de.cketti.unicode:kotlin-codepoints-deluxe:0.6.1")
